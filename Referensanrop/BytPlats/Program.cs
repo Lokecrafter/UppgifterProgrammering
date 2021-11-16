6a45d5dda69c55fb52a704602ae6a5824f32c1ac
Loke Lindberg Nilsson <66024236+Lokecrafter@users.noreply.github.com>
2021-11-17 00:44:19 +0100

    Gjorde de två uppgifterna från presentationen

---
+++ Referensanrop/BytPlats/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BytPlats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programmet kommer att byta plats på variablerna x och y.");
+            Console.Write("Skriv värdet på x: ");
+            string xString = Console.ReadLine();
+            Console.Write("Skriv värdet på y: ");
+            string yString = Console.ReadLine();
+            int x = int.Parse(xString);
+            int y = int.Parse(yString);
+            
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("y: " + y);
+
+            SwitchPlace(ref x, ref y);
+
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("y: " + y);
+        }
+
+        static void SwitchPlace (ref int a, ref int b)
+        {
+            Console.WriteLine("Byter plats...");
+            a += b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("Platser bytta!");
+        }
+    }
+}
---
+++ Referensanrop/Exponent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exponent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 upphöjt till 7: "+Power(10,7));
+	        Console.WriteLine("2 upphöjt till 10: "+Power(2, 10));
+        }
+        ///<param name="b"> Bas. Flyttal, vilket somhelst</param>
+        ///<param name="e"> Exponent. Heltal, endast positivt eller 0</param>
+        static float Power(float b, int e){
+            float returnNumb = 1;
+            for(int i = 0; i < e; i++){
+                returnNumb *= b;
+            }
+            return returnNumb;
+        }
+    }
+}
